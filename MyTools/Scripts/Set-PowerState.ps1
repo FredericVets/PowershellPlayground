15eee9c2ebc985d61cc5aaf3d08855399c004b8c
Frederic Vets <frederic.vets@gmail.com>
2018-05-15 19:09:07 +0200

    Restructured the module

---
+++ MyTools/Scripts/Set-PowerState.ps1
@@ -0,0 +1,11 @@
+<# TODO
+
+Suspend, Hibernate computer
+see https://stackoverflow.com/questions/20713782/suspend-or-hibernate-from-powershell?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+
+Create alias hibernate, suspend
+Different sub-module of MyTools -> power?
+function Set-PowerState {
+
+}
+#>
